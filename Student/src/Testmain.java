34a647dcda6ddfa0f924671cdb1de03e3f6feed7
punamsangole <131657320+punamsangole@users.noreply.github.com>
2024-01-27 16:41:46 +0530

    add changes

--- Student/src/Testmain.java
+++ Student/src/Testmain.java
@@ -2,6 +2,9 @@
 public class Testmain {
 	public static void main(String[] args) {
 		System.out.println("shraddha");
+		System.out.println("shraddha");
+		System.out.println("shraddha");
+		System.out.println("shraddha");
 	}
 
 }
